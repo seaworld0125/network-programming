1747dbf645316ce3ef09f3d824d3e152ee8c2360
Tae-Kyoung Kim <75168305+seaworld0125@users.noreply.github.com>
2022-03-28 23:35:35 +0900

    study

--- HW4_TAEKYOUNG_KIM/server.py
+++ HW4_TAEKYOUNG_KIM/server.py
@@ -1,41 +1,54 @@
+from email import header
+import errno
 import mimetypes
 from socket import *
 
-sock = socket(AF_INET, SOCK_STREAM)
-sock.settimeout(5)
-sock.bind(('', 80))
-sock.listen(5)
-print('waiting...\n')
+def createData(fname):
+    try:
+        mimeType = mimetypes.guess_type(fname)[0]
+        if mimeType == 'text/html':
+            f = open(fname, 'r', encoding='UTF-8')
+        else:
+            f = open(fname, 'rb')
+    except Exception as e:
+        print('not found error')
+        header_ = 'HTTP/1.1 404 Not Found\r\n\r\n'.encode()
+        body_ = '<HTML><HEAD><TITLE>Not Found</TITLE></HEAD><BODY>Not Found</BODY></HTML>'.encode('euc-kr')
+        CNotFoundErr = True
+    else:
+        header_ = ('HTTP/1.1 200 OK/r/n' + 'Content-Type: ' + mimeType + '\r\n\r\n').encode()
+        body_ = f.read()
+        if mimeType == 'text/html':
+            body_ = body_.encode('euc-kr')
+        CNotFoundErr = False
+    finally:
+        return header_, body_, CNotFoundErr
 
-try:
+def clientConnect(sock):
     while True:
         client, addr = sock.accept()
-        
-        data = client.recv(1024).decode().split(' ')
+        data = client.recv(1024)
+        if len(data) == 0: 
+            client.close()
+            break
+
+        data = data.decode().split(' ')
         fname = data[1][1:]
         print('recv request : ' + fname)
 
-        try:
-            mimeType = mimetypes.guess_type(fname)[0]
-            if mimeType == 'text/html':
-                f = open(fname, 'r', encoding='UTF-8')
-            else:
-                f = open(fname, 'rb')
-        except Exception as e:
-            print('Err : ' + e)
-            header_ = 'HTTP/1.1 404 Not Found\r\n\r\n'.encode()
-            body_ = '<HTML><HEAD><TITLE>Not Found</TITLE></HEAD><BODY>Not Found</BODY></HTML>'.encode()
-        else:
-            header_ = ('HTTP/1.1 200 OK/r/n' + 'Content-Type: ' + mimeType + '\r\n\r\n').encode()
-            body_ = f.read()
-            if mimeType == 'text/html':
-                body_ = body_.encode('euc-kr')
-        finally:
-            client.send(header_)
-            client.send(body_)
-            client.close()
+        header_, body_, CNotFoundErr = createData(fname)
+        client.send(header_)
+        client.send(body_)
+        client.close()
+        if CNotFoundErr: break
+
+sock = socket(AF_INET, SOCK_STREAM)
+sock.bind(('', 80))
+sock.listen(5)
+sock.settimeout(5)
+try:
+    clientConnect(sock)
 except:
-    print('\nShut down the server because there are no more requests')
+    print('occur socket.timeout, socket close')
 finally:
     sock.close()
-
