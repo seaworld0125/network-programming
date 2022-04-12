6bdff0a3551d7a3d26ce3c25780125a455de0569
Tae-Kyoung Kim <mabrother98@gmail.com>
2022-04-12 13:38:41 +0900

    study

---
+++ HW9_TAEKYOUNG_KIM/race_rock_cond.py
@@ -0,0 +1,32 @@
+import threading
+
+x = 0
+
+def increament():
+    global x 
+    x += 1
+
+def thread_task(lock):
+    for _ in range(300000):
+        lock.acquire()
+        increament()
+        lock.release()
+
+def main_task():
+    global x 
+    x = 0
+
+    lock = threading.Lock()
+
+    t1 = threading.Thread(target=thread_task(lock))
+    t2 = threading.Thread(target=thread_task(lock))
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
+
+for i in range(10):
+    main_task()
+    print('Iteration {0}: x = {1}'.format(i, x))
